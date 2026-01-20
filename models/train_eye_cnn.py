Fix: Resolved closed-eye misclassification by adding normalization and batch norm to EyeCNN

models/train_eye_cnn.py
@@ -8,6 +8,8 @@ import numpy as np
 X = np.load("Data/Eyes/X.npy")
 y = np.load("Data/Eyes/y.npy")
 
+X = X.astype('float32')/255.0
+
 X = torch.tensor(X, dtype=torch.float32)
 y = torch.tensor(y, dtype=torch.long)
 
@@ -19,25 +21,41 @@ class EyeCNN(nn.Module):
     def __init__(self):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Conv2d(1, 16, 3),
+            # Layer 1: Captures basic edges
+            nn.Conv2d(1, 16, 3, padding=1),
+            nn.BatchNorm2d(16), 
             nn.ReLU(),
             nn.MaxPool2d(2),
-            nn.Conv2d(16, 32, 3),
+            
+            # Layer 2: Captures eye shapes (circle vs flat line)
+            nn.Conv2d(16, 32, 3, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2),
+            
+            # Layer 3: Deeper features for fatigue detection
+            nn.Conv2d(32, 64, 3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            
             nn.Flatten(),
-            nn.Linear(32 * 14 * 14, 64),
+            # Fully connected layers
+            nn.Linear(64 * 8 * 8, 128),
             nn.ReLU(),
-            nn.Linear(64, 2)
+            nn.Dropout(0.5), # Prevents the model from just "memorizing"
+            nn.Linear(128, 2)
         )
 
     def forward(self, x):
         return self.net(x)
 
 model = EyeCNN()
-criterion = nn.CrossEntropyLoss()
+weights = torch.tensor([1.0, 1.0], dtype=torch.float32)
+criterion = nn.CrossEntropyLoss(weight=weights)
+# ----------------------------
+# Define the optimizer
 optimizer = optim.Adam(model.parameters(), lr=0.001)
-
 # Training loop
 for epoch in range(12):
     total_loss = 0